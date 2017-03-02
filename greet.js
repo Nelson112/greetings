eeeb5bc24297d4908dad4c93aff5ba3e384cddb5
Nelson112 <nelsonmgemgwana@gmail.com>
2017-03-02 16:04:26 +0200

    my greetings

---
+++ greet.js
@@ -0,0 +1,6 @@
+function myfunction(){
+  var inputName = document.getElementById("myName").value;
+  document.getElementById("outputgr").innerHTML= "Hello, " + inputName;
+
+  var greet = document.getElementById("myName").value = ""
+}
