e0f65f22b359ff27f62220d899deaf3fa1e9e198
Nelson112 <nelsonmgemgwana@gmail.com>
2017-03-03 16:01:40 +0200

    updated greetings with languages

--- greet.js
+++ greet.js
@@ -1,6 +1,33 @@
 function myfunction(){
-  var inputName = document.getElementById("myName").value;
-  document.getElementById("outputgr").innerHTML= "Hello, " + inputName;
+  var inputName = "";
+var language = document.getElementsByName('language');
 
-  var greet = document.getElementById("myName").value = ""
+  if (language[0].checked) {
+
+  inputName = document.getElementById("myName");
+  document.getElementById("outputgr").innerHTML= "Hello, " + inputName.value;
+
+   var greet = document.getElementById("myName").value = ""
 }
+
+else if (language[1].checked) {
+
+    inputName = document.getElementById("myName");
+  document.getElementsByName("language").value;
+  document.getElementById("outputgr").innerHTML= "Molo, " + inputName.value;
+
+   var greet = document.getElementById("myName").value = ""
+
+}
+else if (language[2].checked) {
+
+    inputName = document.getElementById("myName");
+  document.getElementsByName("language").value;
+  document.getElementById("outputgr").innerHTML= "Hodi, " + inputName.value;
+
+   var greet = document.getElementById("myName").value = ""
+
+}
+return inputName;
+}
+myfunction();
