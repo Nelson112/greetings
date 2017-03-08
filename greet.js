97bf6bd94b2b4c75bd7fb49a8ef6ce05d03bc0e6
Nelson112 <nelsonmgemgwana@gmail.com>
2017-03-08 16:54:46 +0200

    final updates to greet exercise

--- greet.js
+++ greet.js
@@ -1,40 +1,61 @@
 /*declare counter variable to store the number of counts*/
-var counter = 0;
+var greetCounter = 0;
+
+var storedCounter = localStorage.getItem('counter');
+if (storedCounter) {
+  greetCounter = Number(storedCounter)
+}
+
 /*onclick of the greet me button this function will execute*/
-function myfunction(){
+function myfunction() {
+
   /*stored the language radio buttons*/
-var language = document.getElementsByName('language');
-/*declared the x variable to store the input of the name typed in the text box*/
-var x = document.getElementById("myName").value;
+  var language = document.getElementsByName('language');
+  /*declared the x variable to store the input of the name typed in the text box*/
+  var x = document.getElementById("myName").value;
+
+  if ((language[0].checked || language[1].checked || language[2].checked) &&  x.length > 0) {
+    greetCounter += 1
+    document.getElementById("counter").innerHTML = greetCounter;
+
+  }
+  /*stored the language radio buttons*/
+  var language = document.getElementsByName('language');
+
   /*if the english radio button is checked and there is a name typed in the text box, execute this code*/
   if (language[0].checked && x.length > 0) {
     /*stores what ever name that has been typed in the text box*/
-  inputName = document.getElementById("myName").value;
-  document.getElementById("outputgr").innerHTML= "Hello, " + inputName;
-  /*incremates the counter by one*/
-  document.getElementById("counter").innerHTML = counter += 1;
-  /*clears the text box*/
-   var greet = document.getElementById("myName").value = ""
-}
-
-else if (language[1].checked  && x.length > 0) {
+    inputName = document.getElementById("myName").value;
+    document.getElementById("outputgr").innerHTML = "Hello, " + inputName;
+    /*incremates the counter by one*/
+    /*clears the text box*/
+    document.getElementById("myName").value = ""
+  } else if (language[1].checked && x.length > 0) {
 
     inputName = document.getElementById("myName").value;
-  document.getElementsByName("language").value;
-  document.getElementById("outputgr").innerHTML= "Molo, " + inputName;
-  document.getElementById("counter").innerHTML = counter += 1;
-   var greet = document.getElementById("myName").value = ""
+    document.getElementById("outputgr").innerHTML = "Molo, " + inputName;
+    document.getElementById("myName").value = ""
 
-}
-else if (language[2].checked  && x.length > 0) {
+  } else if (language[2].checked && x.length > 0) {
 
     inputName = document.getElementById("myName").value;
-  document.getElementsByName("language").value;
-  document.getElementById("outputgr").innerHTML= "Hodi, " + inputName;
-document.getElementById("counter").innerHTML = counter += 1;
-   var greet = document.getElementById("myName").value = ""
+    document.getElementById("outputgr").innerHTML = "Hodi, " + inputName;
+    document.getElementById("myName").value = ""
+  }
+
+  localStorage.setItem('counter', greetCounter);
+  return inputName;
 
 }
-return inputName;
+
+document.getElementById('counter').innerHTML = greetCounter;
+//myfunction();
+function resetFunction() {
+  var counter = 0;
+  greetCounter = 0;
+  //console.log(greetCounter);
+  localStorage.setItem('counter', 0);
+
+  document.getElementById("counter").innerHTML = greetCounter;
+
 }
-myfunction();
